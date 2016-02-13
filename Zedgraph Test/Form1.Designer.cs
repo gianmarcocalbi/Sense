ed09472c5da8eb7a4c07a66dd9208a6b60540f38
grimidev <calbigianmarco@gmail.com>
2016-02-13 23:52:19 +0100

    ZedGraph Test Created

---
+++ Zedgraph Test/Form1.Designer.cs
@@ -0,0 +1,23 @@
+﻿namespace Zedgraph_Test {
+	partial class Form1 {
+		/// <summary>
+		/// Variabile di progettazione necessaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Pulire le risorse in uso.
+		/// </summary>
+		/// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		private ZedGraph.ZedGraphControl zedGraphControl1;
+		private ZedGraph.ZedGraphControl zedGraphControl2;
+	}
+}
+
