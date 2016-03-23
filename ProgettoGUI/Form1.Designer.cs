dea92ebfaebfbf0434faaf90f95cf04957550c6a
grimidev <calbigianmarco@gmail.com>
2016-03-23 12:31:15 +0100

    Form Property Edited.

--- ProgettoGUI/Form1.Designer.cs
+++ ProgettoGUI/Form1.Designer.cs
@@ -539,8 +539,7 @@
 			// 
 			// Form1
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
 			this.ClientSize = new System.Drawing.Size(1006, 564);
 			this.Controls.Add(this.splitContainer1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
